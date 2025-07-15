262c1bf2531b31fce0a3012ca4b54bff2288c0a5
Maxi <maxi.cac17@gmail.com>
2025-07-15 19:36:27 -0300

    configuracion

--- config/database.py
+++ config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+mysqlconnector://root:admin@localhost:3306/tienda"
+DATABASE_URL = "mysql+mysqlconnector://root:Maxi123@localhost:3306/tienda_tp5"
 
 engine = create_engine(DATABASE_URL)
 
